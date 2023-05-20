b785554e8d763bfb8394e4f269720b9d3a467ec1
ryder <ryderzrashid@gmail.com>
2023-05-20 16:00:49 +0100

    equality operators q

--- fundamentals_1/js_using_node/hello_world.js
+++ fundamentals_1/js_using_node/hello_world.js
@@ -93,10 +93,12 @@ const prompt = require('prompt-sync')();
 
 const numNeighbours = prompt('How many neighbour countries does your country have? ');
 
-if (numNeighbours === '1') {
+if (Number(numNeighbours) === 1) {
   console.log('Only 1 border');
-} else {
+} else if (numNeighbours > 1) {
   console.log('More than 1 border');
+} else {
+    console.log('No borders');
 }
 
 

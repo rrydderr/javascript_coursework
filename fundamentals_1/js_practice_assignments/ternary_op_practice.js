018461355b17f2e1f0353c8d4e3642f57264a884
ryder <ryderzrashid@gmail.com>
2023-05-24 10:42:54 +0100

    extra questions

--- fundamentals_1/js_practice_assignments/ternary_op_practice.js
+++ fundamentals_1/js_practice_assignments/ternary_op_practice.js
@@ -1,3 +1,4 @@
+
 //Question 1:
 //Write a ternary operator that assigns the value "Even" to the variable result if the number num is even, and "Odd" if it's odd.
 
@@ -42,5 +43,81 @@ console.log(`${person === 'male' ? 'Mr' : 'Ms'} Smith is a female.`);
 const capacity = "7";
 
 console.log(capacity >= 50 ? 'Full' : 'Available');
+
+//Question 6
+//Write a ternary operator to determine whether a given year year is a leap year. Assign the value "Leap year" to the variable result if it's a leap year, and "Not a leap year" otherwise.
+//(Hint: A leap year is divisible by 4, but not divisible by 100 unless it's also divisible by 400.)
+
+const year = 2012;
+
+console.log(((year % 4) === 0 && (year % 100) ===!0) || ((year % 4)=== 0 && (year % 100) === 0 && (year % 400)===0) ? 'Leap Year' : 'Not a Leap Year');
+
+
+//Question 7
+//rewrite the following using a ternary operator:
+
+// let num1 = 10;
+// let num2 = 5;
+// let result;
+
+// if (num1 > num2) {
+//   result = "Num1 is greater than Num2";
+// } else if (num1 < num2) {
+//   result = "Num1 is less than Num2";
+// } else {
+//   result = "Num1 is equal to Num2";
+// }
+
+const num1 = 26;
+const num2 = 15; 
+
+const result = console.log(num1> num2 ? 'Num1 is greater than Num2' : num2 > num1 ? 'Num1 is less than Num 2' : 'Num1 is equal to Num2');
+
+
+
+//Question 8
+//Write a ternary operator that checks if a given string str is palindrome or not. Assign the value "Palindrome" to the variable result if it's a palindrome, and "Not a palindrome" otherwise.
+// (Hint: A palindrome is a word or phrase that reads the same backward as forward.)
+
+const string = 'madam';
+
+console.log(string === string.split("").reverse().join("")? "Palindrome" : "Not Palindrome");
+
+
+
+
+//Question 9
+//turn the nested if else statement into a ternary operator
+
+// let num = 12;
+// let result1;
+
+// if (num > 0) {
+//   result = "Positive";
+// } else {
+//   if (num < 0) {
+//     result = "Negative";
+//   } else {
+//     result = "Zero";
+//   }
+// }
+
+const num = 0;
+
+const result1 = console.log(num > 0 ? 'Positive' : num < 0 ? 'Negative' : 'Zero');
+
+
+//Question 10
+// Write a ternary operator to determine the grading of a student based on their score as follows:
+
+// If the score is greater than or equal to 90, assign "A" to the variable grade.
+// If the score is between 80 and 89, assign "B".
+// If the score is between 70 and 79, assign "C".
+// If the score is between 60 and 69, assign "D".
+// For any other score, assign "F".
+
+const score = 77;
+
+const grade = console.log(score >= 90 ? 'A' : score >= 80 && score < 90 ? 'B' : score >= 70 && score < 80 ? 'C' : score >= 60 && score < 70 ? 'D' : 'F');
 
 
